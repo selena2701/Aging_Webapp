3c0ca29e812237b6e49f2b6128d3c0ca933d8e51
Le Thi Hong Cuc <cuclth2701@gmail.com>
2023-05-10 12:13:44 +0700

    commit

--- Aging/src/app/app.component.ts
+++ Aging/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kms-imagefinder';
+  title = 'face-aging';
   constructor(private router: Router) { }
   headerFooter:any='';
   ngOnInit() {
